60248f33ef76148c0bc05f6068c4067fb2c15ca8
yxmanyxman <498571187@qq.com>
2017-02-24 12:47:31 +0800

    node爬虫demo，爬慕课网node教程单页面

---
+++ imooc/http/crawler.js
@@ -0,0 +1,56 @@
+/**
+ * Created by Administrator on 2017/2/24.
+ */
+var http = require('http');
+var url = 'http://www.imooc.com/learn/348';
+var cheerio = require('cheerio');
+
+function filterChapters(html) {
+    var $ = cheerio.load(html);
+    var chapters = $('.chapter');
+
+    var courseData = [];
+    chapters.each(function (item) {/*遍历全部chapters，拿出每一个单独的chapter*/
+        var chapter = $(this);
+        var chapterTitleStr = chapter.find('strong').first().text();
+        var info = chapter.find('.chapter-content').text();
+        var chapterTitle = chapterTitleStr.split(info)[0].trim();
+        var videos = chapter.find('.video').children('li');/*ul列表*/
+        var chapterData = {
+            chapterTitle: chapterTitle,
+            videos: []
+        };
+        videos.each(function (item) {/*遍历全部videos即ul列表，拿出每一个单独的video即li*/
+            var video = $(this).find('a');
+            var videoTitle = video.text();
+            var id = video.attr('href').split('video/')[1];/*split分割成2段后，拿第二段的内容*/
+            chapterData.videos.push({
+                title: videoTitle,
+                id: id
+            })
+        });
+        courseData.push(chapterData);
+    });
+    return courseData;
+}
+function printCourseInfo (courseData){
+    courseData.forEach(function (item) {
+        var chapterTitle = item.chapterTitle;
+        console.log(chapterTitle + '\n');
+        item.videos.forEach(function (video) {
+            console.log('【' + video.id + '】' + video.title +'\n');
+        })
+    })
+}
+http.get(url, function (res) {
+    var html = '';
+    res.on('data', function (data) {
+        html += data
+    });
+    res.on('end', function () {
+        var courseData = filterChapters(html);
+        printCourseInfo(courseData)
+    });
+}).on('error', function () {
+    console.log('出错')
+});
